48cc2dd9a125d4c3632b504506426e5336dece66
Peter <petersrood@gmail.com>
2025-11-18 12:54:28 +0000

    added initialize_pose function

--- pose_GA/GA_functions.py
+++ pose_GA/GA_functions.py
@@ -1,5 +1,22 @@
+from math import fabs, pi
+import random as rd
+
+
 def initialize_pose():
-    pass
+    pose = []
+
+    for _ in range(8):
+        # Upon testing different possible angles for different joints, the following ranges make the most sense to use:
+        # Coxa = -pi/4 to pi/4. Allows the leg to move left and right with respect to the body without the leg crossing into the spider's body.
+        # Femur = -pi/2 to 0. Allows the leg to curl naturally without 'breaking' the joint by bending backwards.
+        # Tibia = -pi/4 to 0. Same reasoning as Femur, but for the Tibia segment.
+        coxa_angle = rd.uniform(-pi / 4, pi / 4)
+        femur_angle = rd.uniform(-pi / 2, 0)
+        tibia_angle = rd.uniform(-pi / 4, 0)
+
+        pose.extend([coxa_angle, femur_angle, tibia_angle])
+
+    return pose
 
 
 def fitness_pose(pose):
